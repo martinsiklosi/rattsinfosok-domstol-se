a86f38ef928fbddb868e536f595f6cf4bdc5fa28
Martin Siklosi <martin@siklosi.se>
2023-11-13 21:54:04 +0100

    Changed to scrape multiple reports

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -1,5 +1,5 @@
 from contextlib import contextmanager
-from time import sleep
+import os
 
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
@@ -29,48 +29,52 @@ def get_frame(page):
 def load_results(iframe, start_date: str, end_date: str) -> None:
     """Load the results in the iframe.
     start_date and end_date must be in the format yyyy-mm-dd."""
-    
+
     # Select all courts
     dropdown_value = "ALLAMYND"
     dropdown_selector = "#select-domstolmyndighet"
     iframe.wait_for_selector(dropdown_selector)
     iframe.select_option(dropdown_selector, dropdown_value)
-    
+
     # Enter start date
     start_date_selector = "#falt-avgorandedatum-fran"
     iframe.wait_for_selector(start_date_selector)
     iframe.fill(start_date_selector, start_date)
-    
+
     # Enter end date
     end_date_selector = "body > form > div:nth-child(5) > div > div > div:nth-child(1) > div:nth-child(2) > input[type=text]:nth-child(3)"
     iframe.wait_for_selector(end_date_selector)
     iframe.fill(end_date_selector, end_date)
-    
+
     # Press search
     search_button_selector = "body > form > div:nth-child(5) > div > div > div:nth-child(2) > div.formularKnappOmrade > button"
     iframe.wait_for_selector(search_button_selector)
     iframe.click(search_button_selector)
-    
+
     # Wait for the results to load
-    results_selector = "body > form > div.centrerad > table"    
+    results_selector = "body > form > div.centrerad > table"
     iframe.wait_for_selector(results_selector)
 
 
-def click_on_first_result(iframe) -> None:
+def get_report_handles(iframe):
     """Clicks on the first result."""
     # All of the result rows are either the class "forstaRad" or "andraRad"
     class1 = "forstaRad"
     class2 = "andraRad"
     xpath_expression = f"//tr[contains(@class, '{class1}') or contains(@class, '{class2}')]"
-    
-    # Get the row
-    row_handle = iframe.wait_for_selector(f"xpath={xpath_expression}")
-    assert row_handle is not None
-    
+
+    # Get the rows
+    iframe.wait_for_selector(f"xpath={xpath_expression}")
+    row_handles = iframe.query_selector_all(f"xpath={xpath_expression}")
+    assert row_handles is not None
+
     # The report is found in the last column
-    link_handle = row_handle.query_selector("td:last-child a")
-    assert link_handle is not None
-    link_handle.click()
+    report_handles = [row_handle.query_selector("td:last-child a")
+                      for row_handle in row_handles
+                      if row_handle]
+    # Get rid of None values
+    report_handles = [rh for rh in report_handles if rh]
+    return report_handles
 
 
 def extract_report_html_from_popup(popup) -> str:
@@ -80,7 +84,7 @@ def extract_report_html_from_popup(popup) -> str:
     iframe = popup.frame(name=report_iframe_name)
     assert iframe is not None
     return iframe.content()
-    
+
 
 def get_text_from_html(html: str) -> str:
     soup = BeautifulSoup(html, features="lxml")
@@ -88,7 +92,18 @@ def get_text_from_html(html: str) -> str:
     return text
 
 
-def main(filename: str, headless: bool, slow_mo: float):
+def scrape_report(report_link, page) -> str:
+    """Click on report and srape the text."""
+    with page.expect_popup() as popup_info:
+        report_link.click()
+    popup = popup_info.value
+    report_html = extract_report_html_from_popup(popup)
+    report_text = get_text_from_html(report_html)
+    popup.close()
+    return report_text
+
+
+def main(max_reports: int, headless: bool, slow_mo: float):
     with playwright_browser(headless=headless, slow_mo=slow_mo) as browser:
         page = browser.new_page()
         page.goto("https://rattsinfosok.domstol.se/lagrummet/")
@@ -102,24 +117,25 @@ def main(filename: str, headless: bool, slow_mo: float):
         # Load the results
         load_results(iframe, start_date="2020-01-01", end_date="2021-01-01")
 
-        # We can now get the html of the results page and scrape information
-        results_html = iframe.content()
-        
-        # For this demo i will just scrape the first result
-        with page.expect_popup() as popup_info:
-            click_on_first_result(iframe)
-        popup = popup_info.value
+        # Lets grab the links to reports
+        report_handles = get_report_handles(iframe)
 
-        report_html = extract_report_html_from_popup(popup)
-        report_text = get_text_from_html(report_html)
-        
-        with open(filename, "w", encoding="utf8") as file:
-            print(report_text, file=file)
-            
-        sleep(slow_mo / 1000) # Allows us to see the popup
-        popup.close() # Make sure to close each popup before moving on to the next to save memory
-        sleep(slow_mo / 1000)
+        # Before we save any files lets make sure the folder exists
+        foldername = "reports"
+        if not os.path.exists(foldername):
+            os.makedirs(foldername)
+
+        # For this demo i will just scrape the first 10 results
+        for report_handle in report_handles[:max_reports]:
+            report_text = scrape_report(report_handle, page)
+
+            report_name = report_handle.text_content()
+            assert report_name is not None
+            filename = report_name.strip()
+
+            with open(f"{foldername}/{filename}.txt", "w", encoding="utf8") as file:
+                print(report_text, file=file)
 
 
 if __name__ == "__main__":
-    main(filename="report.txt", headless=False, slow_mo=400)
+    main(max_reports=10, headless=False, slow_mo=400)
